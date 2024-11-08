e1ad7db652e2d3488e6a9fc1cfe0d3e54956e0a0
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2024-11-08 14:55:02 +0100

    TP terminer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 // TODO
+//Changer la couleur du text
+
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+if (aMettreEnRouge) {
+    aMettreEnRouge.style.color = "red";
+}
+
+//Réagir à un click
+
+const EnRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+
+EnRougeSuiteAClick.addEventListener("click", (evt) => {
+    EnRougeSuiteAClick.style.color = "red";
+});
+
+//collections d'éléments
+
+const collH2 = document.querySelectorAll("h2")
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red";
+    });
+});
